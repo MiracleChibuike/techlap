a171280635fc6b1f3c1594fd58d3250e68daea52
MiracleChibuike <miraclechibuike27@gmail.com>
2026-07-02 13:31:21 +0100

    update page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,28 +8,28 @@ import { Links } from "react-router-dom";
 
 const NavBar = () => {
     const navigate = useNavigate();
-    const menuRef = useRef(null);
-    const navsRef = useRef(null);
-   useEffect(() => {
-    const menu = menuRef.current;
+//     const menuRef = useRef(null);
+//     const navsRef = useRef(null);
+//    useEffect(() => {
+//     const menu = menuRef.current;
     
-    const handleClick = () => {
-        if (navsRef.current) {
-            navsRef.current.classList.toggle("show");
-        }
-    };
+//     const handleClick = () => {
+//         if (navsRef.current) {
+//             navsRef.current.classList.toggle("show");
+//         }
+//     };
 
-    if (menu) {
-        menu.addEventListener("click", handleClick);
-    }
+//     if (menu) {
+//         menu.addEventListener("click", handleClick);
+//     }
 
-    // Cleanup function
-    return () => {
-        if (menu) {
-            menu.removeEventListener("click", handleClick);
-        }
-    };
-}, []);
+//     // Cleanup function
+//     return () => {
+//         if (menu) {
+//             menu.removeEventListener("click", handleClick);
+//         }
+//     };
+// }, []);
 
 return (
   <>
@@ -38,9 +38,9 @@ return (
       <h1 className="logo">
         Tech<span>Lap.</span>
       </h1>
-      <div className="menuIcon">
+      {/* <div className="menuIcon">
         <svg
-          ref={menuRef}
+          // ref={menuRef}
           id="showMenu"
           xmlns="http://www.w3.org/2000/svg"
           height="34"
@@ -49,83 +49,126 @@ return (
           fill="#FFF">
           <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
         </svg>
-      </div>
+      </div> */}
       {/* Nav Bar */}
-      <nav className="navbar" ref={navsRef}>
+      <nav className="navbar">
         <ul>
           <li className="landing">
             <a href="index.html" id="pageHome">
               {" "}
-              <i className="fa-solid fa-house"></i>Home
+              <button className="btn">
+                <i className="fa-solid fa-house"></i>
+              </button>
             </a>
           </li>
           <li>
-            <a href="#about">
-              {" "}
-              <i className="fa-solid fa-users"></i> About Us
-            </a>
+            {" "}
+            <button className="btn">
+              <i className="fa-solid fa-users"></i>
+            </button>
           </li>
           <li>
             <Link to={"/product-listings"}>
-              <i className="fa-solid fa-laptop"></i> <span>Products</span>
+              <button className="btn">
+                {" "}
+                <i className="fa-solid fa-laptop"></i>{" "}
+              </button>
             </Link>
           </li>
           <li>
-            <a href="#shop">
+            <button className="btn">
               {" "}
-              <i className="fa-solid fa-cart-shopping"></i>Shop
-            </a>
-          </li>
-          {/* <!-- <li>
-                        <button type="button" className="btn btn-primary position-relative">
-                            Inbox
-                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                99+
-                                <span className="visually-hidden">unread messages</span>
-                            </span>
-                        </button>
-                    </li> --> */}
-          <li>
-            <a href="#contact">
-              <i className="fa-solid fa-square-phone-flip"></i>Contact
-            </a>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </button>
           </li>
           <li>
-            <a href="#pag" id="pages">
-              Pages <i className="fa-solid fa-caret-down"></i>
-            </a>
-            <div className="drop">
-              <ul>
-                <li>
-                  <a href="#">Appointments</a>
-                </li>
-                <li>
-                  <a href="#">Our Services</a>
-                </li>
-                <li>
-                  <a href="#">Teams</a>
-                </li>
-                <li>
-                  <a href="#">Careers</a>
-                </li>
-                <li>
-                  <a href="#">Testimony</a>
-                </li>
-                <li>
-                  <a href="#">FAQS</a>
-                </li>
-              </ul>
-            </div>
+            <button className="btn">
+              <i className="fa-solid fa-square-phone-flip"></i>
+            </button>
           </li>
           <li>
             <Link to={"/signup"}>
               <button id="sign-upBTN">
-                Sign Up <i className="fa-solid fa-arrow-right-to-bracket"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#fff">
+                  <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm146.5-204.5Q340-521 340-580t40.5-99.5Q421-720 480-720t99.5 40.5Q620-639 620-580t-40.5 99.5Q539-440 480-440t-99.5-40.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm100-95.5q47-15.5 86-44.5-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160q53 0 100-15.5ZM523-537q17-17 17-43t-17-43q-17-17-43-17t-43 17q-17 17-17 43t17 43q17 17 43 17t43-17Zm-43-43Zm0 360Z" />
+                </svg>
               </button>
             </Link>
           </li>
         </ul>
       </nav>
+    </div>
+
+    {/* Mobile Navs */}
+    <div className="mobile-navs">
+      <button className="btn-cta-mobile">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#fff">
+          <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+        </svg>
+        <p>
+          <small className="text-white">Home</small>
+        </p>
+      </button>
+      <button className="btn-cta-mobile">
+        {" "}
+        <i className="fa-solid fa-users text-white"></i>
+        <p>
+          <small className="text-white">About Us</small>
+        </p>
+      </button>
+      <button className="btn-cta-mobile">
+        <Link to={"/product-listings"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#fff">
+            <path d="M80-160q-33 0-56.5-23.5T0-240h160q-33 0-56.5-23.5T80-320v-440q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v440q0 33-23.5 56.5T800-240h160q0 33-23.5 56.5T880-160H80Zm428.5-51.5Q520-223 520-240t-11.5-28.5Q497-280 480-280t-28.5 11.5Q440-257 440-240t11.5 28.5Q463-200 480-200t28.5-11.5ZM160-320h640v-440H160v440Zm0 0v-440 440Z" />
+          </svg>
+          <p>
+            <small className="text-white">Products</small>
+          </p>
+        </Link>
+      </button>
+      <button className="btn-cta-mobile">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#fff">
+          <path d="M223.5-103.5Q200-127 200-160t23.5-56.5Q247-240 280-240t56.5 23.5Q360-193 360-160t-23.5 56.5Q313-80 280-80t-56.5-23.5Zm400 0Q600-127 600-160t23.5-56.5Q647-240 680-240t56.5 23.5Q760-193 760-160t-23.5 56.5Q713-80 680-80t-56.5-23.5ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+        </svg>
+        <p>
+          <small className="text-white">Cart</small>
+        </p>
+      </button>
+      <button className="btn-cta-mobile">
+        <Link to={"/signup"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#fff">
+            <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm146.5-204.5Q340-521 340-580t40.5-99.5Q421-720 480-720t99.5 40.5Q620-639 620-580t-40.5 99.5Q539-440 480-440t-99.5-40.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm100-95.5q47-15.5 86-44.5-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160q53 0 100-15.5ZM523-537q17-17 17-43t-17-43q-17-17-43-17t-43 17q-17 17-17 43t17 43q17 17 43 17t43-17Zm-43-43Zm0 360Z" />
+          </svg>
+          <p>
+            <small className="text-white">Account</small>
+          </p>
+        </Link>
+      </button>
     </div>
   </>
 );
--- components/ProductDetails.jsx
+++ components/ProductDetails.jsx
@@ -56,7 +56,7 @@ const ProductDetails = () => {
             ...res.data(),
           });
           console.log(res.data());
-           console.log(product);
+          //  console.log(product);
         } else {
           console.log("No product found for this id.");
         }
@@ -65,7 +65,7 @@ const ProductDetails = () => {
       }
     };
     fetchProductDetails();
-  }, [productId]);
+  }, []);
 
     if (!product) {
       return (
@@ -89,7 +89,12 @@ const ProductDetails = () => {
             <div className="render-safe">
               <div className="products-header-navs d-flex gap-1">
                 <Link to={"/"}>
-                  <p className="" style={{ fontWeight: "lighter" }}>
+                  <p
+                    className="text-secondary"
+                    style={{
+                      fontWeight: "lighter",
+                      fontSize: "clamp(0.2rem, 20.5vw, 0.9rem)",
+                    }}>
                     Home{" "}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -102,7 +107,12 @@ const ProductDetails = () => {
                   </p>
                 </Link>
                 <Link to={"/product-listings"}>
-                  <p style={{ fontWeight: "lighter" }}>
+                  <p
+                    className="text-secondary"
+                    style={{
+                      fontWeight: "lighter",
+                      fontSize: "clamp(0.2rem, 20.5vw, 0.9rem)",
+                    }}>
                     Product Listings{" "}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -114,16 +124,178 @@ const ProductDetails = () => {
                     </svg>{" "}
                   </p>
                 </Link>
-                <p style={{ fontWeight: "lighter" }}>{product.product_name}</p>
+                <p
+                  style={{
+                    fontWeight: "lighter",
+                    fontSize: "clamp(0.2rem, 20.5vw, 1rem)",
+                  }}>
+                  {product.product_name}
+                </p>
               </div>
               <div className="product-render-data mt-4">
                 <div className="p-name_price d-flex justify-content-between">
                   <h3>{product.product_name}</h3>
                   <p aria-label="button">
-                    <button id="price-tag"className="p-1 border-0 rounded-5">
+                    <button id="price-tag" className="p-1 border-0 rounded-5">
                       ${product.price_tag}
                     </button>
                   </p>
+                </div>
+                {/* Product-image-description */}
+                <div className="container-global-description">
+                  <div className="product-img-description-specs">
+                    <div className="product-img-key-features bg-white">
+                      <div className="product-img-display">
+                        <img
+                          id="imageURL"
+                          src={product.imageURL}
+                          alt={product.product_name}
+                          style={{
+                            // background: "#e0d4d4",
+                            borderTopLeftRadius: "25px",
+                            borderTopRightRadius: "25px",
+                            transform: "rotate(355deg)",
+                            position: "relative",
+                            left: "15px",
+                          }}
+                        />
+                      </div>
+                      <div className="key-features p-4">
+                        <h5 className="mb-4" style={{ color: "#d8a84e" }}>
+                          <strong>Key Features</strong>
+                        </h5>
+                        <div className="features-list d-flex gap-3 ">
+                          <div>{product.product_graphics}</div>
+                          <div>{product.product_processor}</div>
+                          <div>{product.product_storage}</div>
+                          <div>{product.product_graphics}</div>
+                          <div>{product.product_battery_life}</div>
+                        </div>
+                      </div>
+                      {/* Cart - Buy Now */}
+                      <div className="cta-btns d-flex gap-4 p-4">
+                        <button id="cart-add-cta-btn" className="btn bg-dark" style={{width: "100%"}}>
+                          <svg
+                          id="svg-cart"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="20px"
+                            viewBox="0 -960 960 960"
+                            width="20px"
+                            fill="#fff">
+                            <path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM223.5-103.5Q200-127 200-160t23.5-56.5Q247-240 280-240t56.5 23.5Q360-193 360-160t-23.5 56.5Q313-80 280-80t-56.5-23.5Zm400 0Q600-127 600-160t23.5-56.5Q647-240 680-240t56.5 23.5Q760-193 760-160t-23.5 56.5Q713-80 680-80t-56.5-23.5ZM40-800v-80h131l170 360h280l156-280h91L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68.5-39t-1.5-79l54-98-144-304H40Z" />
+                          </svg>
+                          Add to Cart
+                        </button>
+                        <button id="btn-buy-now" className="btn" style={{width: "100%"}}>Buy Now</button>
+                      </div>
+                      <div
+                        className="description-large-left p-4 mt-3"
+                        style={{ background: "#2e2e2c", color: "#e0d4d4" }}>
+                        <h4 style={{ color: "#d8a84e" }}>Description</h4>
+                        <p>{product.product_description}</p>
+                      </div>
+                    </div>
+                    {/* Products-main-specifications */}
+                    <div className="specs-container-main bg-white p-4">
+                      <div className="specs-desc mt-3">
+                        <h3>Product Specifications</h3>
+                        <table className="table table-responsive">
+                          <tbody>
+                            <tr>
+                              <td>Product ID:</td>
+                              <td>
+                                <strong>{product.product_id}</strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Brand:</td>
+                              <td>
+                                <strong>{product.product_brand}</strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Series:</td>
+                              <td>
+                                <strong>{product.Product_series}</strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Screen Size:</td>
+                              <td>
+                                <strong>{product.product_size}</strong> inches
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Processor:</td>
+                              <td>
+                                <strong>
+                                  {product.product_processor}
+                                </strong>{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Operating System (OS):</td>
+                              <td>
+                                <strong>{product.product_OS}</strong>{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Graphics (OS):</td>
+                              <td>
+                                <strong>{product.product_graphics} </strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Storage:</td>
+                              <td>
+                                <strong>{product.product_storage} </strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Weight (kg):</td>
+                              <td>
+                                <strong>{product.price_weight}</strong>{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Color:</td>
+                              <td>
+                                <strong>{product.product_color}</strong>{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Warrantee:</td>
+                              <td>
+                                <strong>{product.product_warantee}</strong>{" "}
+                                years{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Manufacturer:</td>
+                              <td>
+                                <strong>
+                                  {product.product_manufacturer}
+                                </strong>{" "}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Condition</td>
+                              <td>
+                                <strong>
+                                  {product.product_condition}
+                                </strong>{" "}
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <h4 style={{ color: "#d8a84e" }}>Description</h4>
+                        <p>{product.product_description}</p>
+                        <div className="products-related mt-5">
+                          <h4>Related Products</h4>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
